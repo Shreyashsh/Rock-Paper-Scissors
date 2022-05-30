79ad4c2d21000cb290039677ccda36e38bf4100d
idkreally989 <shreyashsharma9314@gmail.com>
2022-05-30 13:07:02 +0530

    game is fully functional but no error control yet

--- javascript.js
+++ javascript.js
@@ -2,4 +2,23 @@ function computerPlay(){
     return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay());
+function playRound(computerSelection, playerSelection){
+    if(computerSelection == playerSelection){
+        return 1;
+    } else if ((computerSelection == "ROCK" && playerSelection == "PAPER") || 
+        (computerSelection == "PAPER" && playerSelection == "SCISSORS") || 
+        (computerSelection == "SCISSORS" && playerSelection == "ROCK")){
+            return 0;
+    } else  return 2;
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        let userSelection = prompt("ROCK, PAPER, SCISSORS?").toUpperCase();
+        let computerSelection = computerPlay().toUpperCase();
+        let result = playRound(computerSelection, userSelection);
+        console.log(['You win!', "It's a tie!", 'You lose!'][result]);
+    }
+}
+
+game();
